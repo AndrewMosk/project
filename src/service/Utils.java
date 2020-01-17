8dc5c07b458201853c3c15993fc151925c9ab6d1
Andrew Moskalets <MoskaletsAndrey@yandex.ru>
2020-01-17 12:24:15 +0300

    добавил логику на удаление

--- src/service/Utils.java
+++ src/service/Utils.java
@@ -42,6 +42,30 @@ public class Utils {
         return String.format(sql, n_table, operation, n_table);
     }
 
+    public static String[][] getTablesOperationD() {
+        return new  String[][]  {
+                {"cl", "CL_ADDRESS", "CL_BANK", "CL_CONTACT", "CL_OKVED", "CL_P", "CL_POTR", "CL_POTRV", "CL_PSPIS", "CL_REZ", "CL_MV", "CL_MVNP", "CL_MVPERS", "CL_MVRAB",
+                        "CL_MVSPIS", "CL_Z", "CL_ZSPIS", "CL_ZV", "CL_ZVSPIS", "CL_SR", "CL"},
+
+                {"unstructured", "JOB_DIR", "JOB_REZ", "RK_REZ", "RK_LIST", "CONS"}, // в JOB_DIR есть ссылка на PERS, так что не такой он и неструктурированный
+
+                {"pers", "PERS_BOLN", "PERS_BOOK", "PERS_DEF", "PERS_LANG", "PERS_OR", "PERS_PCL", "PERS_POOR", "PERS_PROF", "PERS_PROFIL", "PERS_REZ", "PERS_SPAR", "PERS_SIELEV", "PERS_SPEC",
+                        "PERS_SPEN", "PERS_STAJ", "PERS_CLOSE", "PERS_REZDOC", "ASK_OR", "ASK_PLIST", "PROF_ASK", "W_ASK", "PERS_DOP", "PERS"},
+
+                {"vac", "VAC_AGR", "VAC_CR", "VAC_DEF", "VAC_DISTR", "VAC_FAIR", "VAC_FAIR_CL", "VAC_FAIR_DEF", "VAC_FAIR_PERS", "VAC_FAIR_PROF", "VAC_FREE_PACK", "VAC_FREE_VAC",
+                        "VAC_HISTORY", "VAC_KVOT", "VAC_KVOT_CONFIRM", "VAC_KVOT_DOC", "VAC_KVOT_RM", "VAC_KVOT_TRUD", "VAC_LANG", "VAC_LG", "VAC_ONV", "VAC_PC", "VAC_SPAR", "VAC_TRV", "VAC"}, //"VAC_CNT",
+
+                {"avac", "AVAC_LG", "AVAC_ONV", "AVAC_TRV", "AVAC"},
+
+                {"agr", "AGR_OR", "AGR_SPIS", "AGR_STAT", "AGR_TABLE"},
+
+                {"prof", "PROF_AGR", "PROF_DIR", "PROF_VAC_CL", "PROF_VAC_GR", "PROF_VAC"},
+
+                {"s", "S_ADEP", "S_ALG", "S_APERIOD", "S_APERIODM", "S_ARABN", "S_ARABU", "S_ARABV", "S_ASPIS", "S_ASPISD", "S_DEP", "S_FIL", "S_FPERS", "S_OTD", "S_PERIOD", "S_PERIODM",
+                        "S_PPS", "S_RABDN", "S_RABDU", "S_RABN", "S_RABU", "S_RABV", "S_SPIS", "S_SPISD", "S_SPNU", "S_SPPROC", "S_STAT", "S_SVODN", "S_SVODU", "S_SVODV", "S_VOZV"}
+        };
+    }
+
     public static String[][] getTables() {
         return new  String[][]  {
                 {"cl", "*CL", "CL_ADDRESS", "CL_BANK", "CL_CONTACT", "CL_OKVED", "CL_P", "CL_POTR", "CL_POTRV", "CL_PSPIS", "CL_REZ", "CL_MV", "CL_MVNP", "CL_MVPERS", "CL_MVRAB",
