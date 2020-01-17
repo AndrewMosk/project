1c9ceca5efa494e13ab4b7adde7e4a67efec5703
Andrew Moskalets <MoskaletsAndrey@yandex.ru>
2020-01-17 16:29:25 +0300

    работаю над логикой

--- src/Test.java
+++ src/Test.java
@@ -2,30 +2,69 @@ import service.Database;
 import service.Mail;
 import service.Utils;
 
+import java.io.IOException;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
-    String str ="ERROR: insert or update on table \"cl_address\" violates foreign key constraint \"cl_address_fk\"\n" +
-            "  Подробности: Key (c_client)=(1310050054) is not present in table \"cl\".\n" +
-            "  Где: SQL statement \"INSERT INTO\n" +
-            "\tcl_address (\"c_address\", \"pr_addr\", \"c_client\", \"c_post\", \"aoid_j\", \"houseid\", \"c_country\", \"c_town\", \"c_street\", \"home\", \"crp\", \"kv\", \"lit\", \"prim_addr\", \"txt_addr\", \n" +
-            "\t\t\"p_modi\", \"d_modi\")\n" +
-            "SELECT \"c_address\", \"pr_addr\", \"c_client\", \"c_post\", \"aoid_j\", \"houseid\", \"c_country\", \"c_town\", \"c_street\", \"home\", \"crp\", \"kv\", \"lit\",\t\"prim_addr\", \"txt_addr\", \n" +
-            "\t\t\"pers_modi\" AS \"p_modi\", \"d_modi\"\n" +
-            "FROM ora_cl_address WHERE ora_cl_address.c_address = '1310048889'\n" +
-            "ON CONFLICT (\"c_address\") DO UPDATE SET \"pr_addr\" = EXCLUDED.pr_addr, \"c_client\" = EXCLUDED.c_client, \"c_post\" = EXCLUDED.c_post, \"aoid_j\" = EXCLUDED.aoid_j, \n" +
-            "\t\t\"houseid\" = EXCLUDED.houseid, \"c_country\" = EXCLUDED.c_country, \"c_town\" = EXCLUDED.c_town, \"c_street\" = EXCLUDED.c_street, \"home\" = EXCLUDED.home, \n" +
-            "\t\t\"crp\" = EXCLUDED.crp, \"kv\" = EXCLUDED.kv, \"lit\" = EXCLUDED.lit, \"prim_addr\" = EXCLUDED.prim_addr, \"txt_addr\" = EXCLUDED.txt_addr, \"p_modi\" = EXCLUDED.p_modi, \n" +
-            "\t\t\"d_modi\" = EXCLUDED.d_modi\"\n" +
-            "PL/pgSQL function inline_code_block line 3 at SQL statement', 'Sun Dec 29 18:32:46 MSK 2019";
+        // взял первый пример сразу удачно - PERS_CLOSE - там ссылка на sl_pers - у меня таких не создано!  нужно добавить!!!
 
-        System.out.println(str.replace("'",""));
+        String error = "ERROR: insert or update on table \"pers_rezdoc\" violates foreign key constraint \"pers_rezdoc_fk\"\n" +
+                "  Подробности: Key (r_rez)=(1805797767) is not present in table \"pers_rez\".\n" +
+                "  Где: SQL statement \"INSERT INTO\n" +
+                "\tpers_rezdoc (\"r\", \"r_rez\", \"c_order\", \"hex\", \"status\", \"p_modi\", \"d_modi\")\n" +
+                "SELECT \"r\", \"r_rez\", \"c_order\", \"hex\", \"status\", \"p_modi\", \"d_modi\"\n" +
+                "FROM ora_pers_rezdoc WHERE ora_pers_rezdoc.r = 1805797767_18\n" +
+                "ON CONFLICT (\"r\") DO UPDATE SET \"r_rez\" = EXCLUDED.r_rez, \"c_order\" = EXCLUDED.c_order, \"hex\" = EXCLUDED.hex, \"status\" = EXCLUDED.status, \n" +
+                "\t\t\"p_modi\" = EXCLUDED.p_modi, \"d_modi\" = EXCLUDED.d_modi\"\n" +
+                "PL/pgSQL function inline_code_block line 3 at SQL statement";
 
+        String[] errorData = parseError(error);
+        String sql = generateSql(errorData[0], errorData[1], errorData[2]);
+
+        System.out.println(sql);
+
+//        for (String s : array) {
+//            System.out.println(s);
+//        }
+
+    }
+
+    private static String generateSql(String r_table, String r, String n_table) throws IOException {
+        // имена всех таблиц, имеют префик, совпадающий с именем папки, в которой они хранятся
+        String folderName = n_table.split("_")[0];
+
+        String dir = "scripts/" + folderName + "/s/" + n_table + ".sql";
+        String sqlQuery = Utils.readSqlQuery(dir);
+
+        return String.format(sqlQuery, r, r);
+    }
+
+    private static String[] parseError(String error) {
+        String[] result = new String[3];
+
+        int index1 = error.indexOf("Подробности:", 0);
+        int index2 = error.indexOf("Где:", 0);
+        error = error.substring(index1, index2);
+
+        String[] split = error.split("=");
+        index1 = split[0].indexOf("(") + 1;
+        index2 = split[0].indexOf(")");
+        result[0] = split[0].substring(index1, index2);
+
+        index1 = split[1].indexOf("(") + 1;
+        index2 = split[1].indexOf(")");
+        result[1] = split[1].substring(index1, index2);
+
+        index1 = split[1].indexOf("table") + 7;
+        index2 = split[1].length() - 5;
+        result[2] = split[1].substring(index1, index2);
+
+        return result;
     }
 
 }
