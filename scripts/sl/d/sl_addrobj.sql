cc9c90c9d6ef07e9b35fb1eb646bf86d97342cc3
Andrew Moskalets <MoskaletsAndrey@yandex.ru>
2020-01-30 16:38:13 +0300

    добавил справочники sl

--- scripts/sl/d/sl_addrobj.sql
+++ scripts/sl/d/sl_addrobj.sql
@@ -1,6 +1,6 @@
 DO $$
 BEGIN
-DELETE FROM sl_addrobj WHERE aoguid IN (select "R_TABLE"::bigint from ora_replog999 WHERE "N_TABLE" = 'SL_ADDROBJ' and "OPER" = 'D');
+DELETE FROM sl_addrobj WHERE aoguid IN (select "R_TABLE" from ora_replog999 WHERE "N_TABLE" = 'SL_ADDROBJ' and "OPER" = 'D');
 DELETE FROM ora_replog999 WHERE "N_TABLE" = 'SL_ADDROBJ' AND "OPER" = 'D';
 END;
 $$ LANGUAGE plpgsql;
--- scripts/sl/d/sl_dgv.sql
+++ scripts/sl/d/sl_dgv.sql
@@ -1,6 +1,6 @@
 DO $$
 BEGIN
-DELETE FROM sl_dgv WHERE rpu_code IN (select "R_TABLE"::bigint from ora_replog999 WHERE "N_TABLE" = 'SL_DVG' and "OPER" = 'D');
+DELETE FROM sl_dgv WHERE rpu_code IN (select "R_TABLE" from ora_replog999 WHERE "N_TABLE" = 'SL_DVG' and "OPER" = 'D');
 DELETE FROM ora_replog999 WHERE "N_TABLE" = 'SL_DVG' AND "OPER" = 'D';
 END;
 $$ LANGUAGE plpgsql;
