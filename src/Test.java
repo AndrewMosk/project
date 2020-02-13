31d08d5be06dde0c7ea68fc9673ae15391718905
Andrew Moskalets <MoskaletsAndrey@yandex.ru>
2020-02-13 16:12:56 +0300

    в тест добавил рекурсивный вызов метода удаления битых ссылок - на боевую пока не прикрутил

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,4 @@
+import org.postgresql.core.SqlCommand;
 import service.Database;
 import service.Utils;
 
@@ -48,6 +49,80 @@ public class Test {
         }
 
 
+//        ----------------------------------------------------------------------------------------------------------------------------------
+//        удаление вложенных цепочек битых ссылок - пока делал, такая ошибка пропала, так что на боевую пока не переношу
+//        StringBuilder sql = new StringBuilder("DO $$\n" +
+//                "BEGIN\n" +
+//                "DELETE FROM cl WHERE c_client IN (select \"R_TABLE\"::bigint from ora_replog999 WHERE \"N_TABLE\" = 'CL' and \"OPER\" = 'D');\n" +
+//                "DELETE FROM ora_replog999 WHERE \"N_TABLE\" = 'CL' AND \"OPER\" = 'D';\n" +
+//                "END;\n" +
+//                "$$ LANGUAGE plpgsql;");
+//
+//
+//
+//        boolean continueLoad = true;
+//        while (continueLoad) {
+//            continueLoad = deleteRows(sql, "CL");
+//        }
+//        ----------------------------------------------------------------------------------------------------------------------------------
+
         Database.close();
+    }
+
+    private static boolean deleteRows(StringBuilder sqlQuery, String table) throws SQLException {
+        String errorSql;
+
+        try {
+            stmt.executeUpdate(sqlQuery.toString());
+        } catch (SQLException e) {
+//            int a = 1;
+
+            if (e.getMessage().contains("violates foreign key constraint")) {
+                deleteForeignKeyConstraint(e.getMessage());
+
+                // ошибка битой ссылки устранена, снова пытаюсь удалить все строки данной таблицы
+                return true;
+            } else if (Utils.transactionError(e.getMessage())) {
+                // если ошибка связана с транзакционностью вызываю метод удаления еще раз
+                return true;
+            } else {
+                // если какая-то другая ошибка (синтаксис или скажем неверный порядок вызова скрипта удаления) тогда запись в лог ошибок и переход к следующему
+                errorSql = Utils.insertToErrorLog("D", table, e.getMessage().replace("'", ""));
+                stmt.executeUpdate(errorSql);
+            }
+        }
+
+        return false;
+    }
+
+    private static void deleteForeignKeyConstraint(String errorMessage) throws SQLException {
+        // распарсить имя таблицы из текста ошибки!
+        String table = getTableFromErrorMessage(errorMessage);
+        // генерирую текст запроса для поиска foreign key
+        String foreignKeySql = Utils.getForeignKeySql(table, Utils.parseErrorGetChildTable(errorMessage));
+        ResultSet rs = stmt.executeQuery(foreignKeySql);
+        rs.next();
+        String foreignKey = rs.getString("t_column");
+        String childTable = rs.getString("t_table");
+        // получаю код строки для удаления
+        foreignKey = foreignKey.substring(1, foreignKey.length() - 1);
+
+        String sqlDelete = Utils.generateSqlDelete(errorMessage, childTable, foreignKey);
+        try {
+            stmt.executeUpdate(sqlDelete);
+        } catch (SQLException e) {
+            deleteForeignKeyConstraint(e.getMessage());
+        }
+    }
+
+    private static String getTableFromErrorMessage(String errorMessage) {
+        String error = "ERROR: update or delete on table \"cl\" violates foreign key constraint \"cl_address_fk\" on table \"cl_address\"\n" +
+                "  Подробности: Key (c_client)=(704094) is still referenced from table \"cl_address\".\n" +
+                "  Где: SQL statement \"DELETE FROM cl WHERE c_client IN (select \"R_TABLE\"::bigint from ora_replog999 WHERE \"N_TABLE\" = 'CL' and \"OPER\" = 'D')\"\n" +
+                "PL/pgSQL function inline_code_block line 3 at SQL statement";
+        String part1 = error.split("ERROR: update or delete on table \"", 2)[1];
+        String part2 = part1.split("\" violates foreign key constraint", 2)[0];
+
+        return part2;
     }
 }
